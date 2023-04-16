fa50e43e63b694f85cbb508c6724514aa45e2e58
nyxspi <nyxspi@gmail.com>
2023-04-16 20:20:03 +0200

    testing testing 123

--- SBS_2page_webApp/SBS_2page_webApp/App_Start/RouteConfig.cs
+++ SBS_2page_webApp/SBS_2page_webApp/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace SBS_2page_webApp
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+            name: "Login",
+            url: "Views/Login.cshtml",
+            defaults: new { controller = "Login", action = "Index" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
